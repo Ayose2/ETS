9f1fd00d877281bbfc811f29ecbbf31e5985d282
Ayose <Ayose@Ayose>
2016-02-24 19:05:24 +0000

    hola

---
+++ ETSGit/src/ejemplos/Adivina.java
@@ -0,0 +1,32 @@
+package ejemplos;
+
+import java.util.Scanner;
+
+public class Adivina {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int genera = (int)(Math.random() * 10 ) + 1;
+		System.out.println("Primer intento(" + genera + ")" + ": ");
+		int numero = teclado.nextInt();
+		if (numero == genera)
+			System.out.println("Correcto.");
+		else {
+			System.out.println("Segundo intento: ");
+			numero = teclado.nextInt();
+			if (numero == genera)
+				System.out.println("Correcto.");
+			else { 
+				System.out.println("Último intento: ");
+				numero = teclado.nextInt();
+				if (numero == genera)
+					System.out.println("Correcto.");
+				else 
+					System.out.println("Lo siento, el número era el " + genera);
+			}
+		}
+		teclado.close();
+		
+	}
+
+}

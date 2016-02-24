9fd94853e4ef01a7508fda336806f023b2e99afc
Ayose2 <ayose44@hotmail.com>
2016-02-24 21:48:08 +0000

    Update Aleatorio.java

--- ETSGit/src/ejemplos/Aleatorio.java
+++ ETSGit/src/ejemplos/Aleatorio.java
@@ -5,19 +5,19 @@ public class Aleatorio {
 	
 	public static void main (String[] args) {
 		
-		//Número aleatorio entre 1 y 6 (lanzamineto de dado)
+		//NÃºmero aleatorio entre 1 y 6 (lanzamineto de dado)
 		int dado = (int)(Math.random() * 6) + 1;
 		System.out.println("El valor es: " + dado + ".");
 		
-		//Número aleatorio entre 10 y 20 ambos incluidos
-		int valor = (int)(Math.random() * 11) + 10;
+		//NÃºmero aleatorio entre 10 y 20 ambos incluidos
+		int valor = (int)(Math.random() * 10) + 10;
 		System.out.println("El valor es: " + valor + ".");
 		
-		//Número aleatorio entre 0 y 1 ambos incluidos
+		//NÃºmero aleatorio entre 0 y 1 ambos incluidos
 		int bit = (int)(Math.random() * 2);
 		System.out.println("El bit es: " + bit + ".");
 		
-		//Número aleatorio entre 1 y 100 ambos incluidos
+		//NÃºmero aleatorio entre 1 y 100 ambos incluidos
 		int valor2 = (int)(Math.random() * 100) + 1;
 		System.out.println("El valor entre 1 y 100 es: " + valor2 + ".");
 		
